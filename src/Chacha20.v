e6179991d2b25b526ec005c40de7afedc3ffc6af
samlu00412 <79298245+samlu00412@users.noreply.github.com>
2026-05-17 12:57:57 +0800

    refactor: modify chacha20 To cut critical path, I modified quarter round logic, dividing the whole round into column round and diagonal round

--- src/Chacha20.v
+++ src/Chacha20.v
@@ -9,14 +9,14 @@ module Chacha20 (
     output wire [511:0]  keystream   
 );
     integer i, j;
-    localparam IDLE = 2'd0, CALC = 2'd1, ADD = 2'd2, DONE = 2'd3;
+    localparam IDLE = 3'd0, CALC_COL = 3'd1, CALC_DIAG = 3'd2, ADD = 3'd3, DONE = 3'd4;
     localparam CONSTANT1 = 32'h61707865, CONSTANT2 = 32'h3320646e, CONSTANT3 = 32'h79622d32, CONSTANT4 = 32'h6b206574;
 
-    reg [1:0] state, next_state;
+    reg [2:0] state, next_state;
     reg [3:0] round_cnt;
     reg [31:0] init_buf[0:15];
     reg [31:0] keystream_buf[0:15];
-
+    reg [31:0] col_buf[0:15];
     wire [31:0] col_out[0:15];
     wire [31:0] diagonal_out[0:15];
 
@@ -40,20 +40,18 @@ module Chacha20 (
                        .d(keystream_buf[15]), .out_a(col_out[3]), .out_b(col_out[7]),
                        .out_c(col_out[11]), .out_d(col_out[15]));
 
-    Quarter_round Diag1(.a(col_out[0]), .b(col_out[5]), .c(col_out[10]),
-                       .d(col_out[15]), .out_a(diagonal_out[0]), .out_b(diagonal_out[5]),
+    Quarter_round Diag1(.a(col_buf[0]), .b(col_buf[5]), .c(col_buf[10]),
+                       .d(col_buf[15]), .out_a(diagonal_out[0]), .out_b(diagonal_out[5]),
                        .out_c(diagonal_out[10]), .out_d(diagonal_out[15]));
-    Quarter_round Diag2(.a(col_out[1]), .b(col_out[6]), .c(col_out[11]),
-                       .d(col_out[12]), .out_a(diagonal_out[1]), .out_b(diagonal_out[6]),
+    Quarter_round Diag2(.a(col_buf[1]), .b(col_buf[6]), .c(col_buf[11]),
+                       .d(col_buf[12]), .out_a(diagonal_out[1]), .out_b(diagonal_out[6]),
                        .out_c(diagonal_out[11]), .out_d(diagonal_out[12]));
-    Quarter_round Diag3(.a(col_out[2]), .b(col_out[7]), .c(col_out[8]),
-                       .d(col_out[13]), .out_a(diagonal_out[2]), .out_b(diagonal_out[7]),
+    Quarter_round Diag3(.a(col_buf[2]), .b(col_buf[7]), .c(col_buf[8]),
+                       .d(col_buf[13]), .out_a(diagonal_out[2]), .out_b(diagonal_out[7]),
                        .out_c(diagonal_out[8]), .out_d(diagonal_out[13]));
-    Quarter_round Diag4(.a(col_out[3]), .b(col_out[4]), .c(col_out[9]),
-                       .d(col_out[14]), .out_a(diagonal_out[3]), .out_b(diagonal_out[4]),
+    Quarter_round Diag4(.a(col_buf[3]), .b(col_buf[4]), .c(col_buf[9]),
+                       .d(col_buf[14]), .out_a(diagonal_out[3]), .out_b(diagonal_out[4]),
                        .out_c(diagonal_out[9]), .out_d(diagonal_out[14]));
-    
-                    
 
     always @(posedge clk or posedge rst) begin
         if(rst) state <= IDLE;
@@ -64,64 +62,47 @@ module Chacha20 (
         if(rst) next_state = IDLE;
         else begin
             case (state)
-                IDLE: next_state = (start) ? CALC : IDLE;
-                CALC: next_state = (round_cnt == 4'd9) ? ADD : CALC; 
-                ADD: next_state = DONE;
-                DONE: next_state = IDLE;
+                IDLE:      next_state = (start) ? CALC_COL : IDLE;
+                CALC_COL:  next_state = CALC_DIAG; 
+                CALC_DIAG: next_state = (round_cnt == 4'd9) ? ADD : CALC_COL; 
+                ADD:       next_state = DONE;
+                DONE:      next_state = IDLE;
+                default:   next_state = IDLE;
             endcase
         end
     end
 
     always @(posedge clk or posedge rst) begin
         if(rst) round_cnt <= 4'd0;
-        else round_cnt <= (state == CALC) ? round_cnt + 4'd1 : 4'd0;
+        else if (state == IDLE) round_cnt <= 4'd0;
+        else if (state == CALC_DIAG) round_cnt <= round_cnt + 4'd1; 
     end
 
     always @(posedge clk) begin
          case (state)
             IDLE: begin
                 if(start) begin
-                    init_buf[0] <= CONSTANT1;
-                    init_buf[1] <= CONSTANT2;
-                    init_buf[2] <= CONSTANT3;
-                    init_buf[3] <= CONSTANT4;
-                    keystream_buf[0] <= CONSTANT1;
-                    keystream_buf[1] <= CONSTANT2;
-                    keystream_buf[2] <= CONSTANT3;
-                    keystream_buf[3] <= CONSTANT4;
+                    init_buf[0] <= CONSTANT1; init_buf[1] <= CONSTANT2;
+                    init_buf[2] <= CONSTANT3; init_buf[3] <= CONSTANT4;
+                    keystream_buf[0] <= CONSTANT1; keystream_buf[1] <= CONSTANT2;
+                    keystream_buf[2] <= CONSTANT3; keystream_buf[3] <= CONSTANT4;
+                    
                     for (i = 0; i < 8; i = i + 1) begin
-                        init_buf[4+i] <= {
-                            key[231 - i*32 -: 8],
-                            key[239 - i*32 -: 8],
-                            key[247 - i*32 -: 8],
-                            key[255 - i*32 -: 8]
-                        };
-                        keystream_buf[4+i] <= {
-                            key[231 - i*32 -: 8],
-                            key[239 - i*32 -: 8],
-                            key[247 - i*32 -: 8],
-                            key[255 - i*32 -: 8]
-                        };
+                        init_buf[4+i] <= {key[231 - i*32 -: 8], key[239 - i*32 -: 8], key[247 - i*32 -: 8], key[255 - i*32 -: 8]};
+                        keystream_buf[4+i] <= {key[231 - i*32 -: 8], key[239 - i*32 -: 8], key[247 - i*32 -: 8], key[255 - i*32 -: 8]};
                     end
                     init_buf[12] <= counter;
                     keystream_buf[12] <= counter;
                     for (j = 0; j < 3; j = j + 1) begin
-                        init_buf[13+j] <= {
-                            nonce[71 - j*32 -: 8],
-                            nonce[79 - j*32 -: 8],
-                            nonce[87 - j*32 -: 8],
-                            nonce[95 - j*32 -: 8] 
-                        };
-                        keystream_buf[13+j] <= {
-                            nonce[71 - j*32 -: 8],
-                            nonce[79 - j*32 -: 8],
-                            nonce[87 - j*32 -: 8],
-                            nonce[95 - j*32 -: 8] 
-                        };
+                        init_buf[13+j] <= {nonce[71 - j*32 -: 8], nonce[79 - j*32 -: 8], nonce[87 - j*32 -: 8], nonce[95 - j*32 -: 8]};
+                        keystream_buf[13+j] <= {nonce[71 - j*32 -: 8], nonce[79 - j*32 -: 8], nonce[87 - j*32 -: 8], nonce[95 - j*32 -: 8]};
                     end
                 end
             end
-            CALC: begin
+            CALC_COL: begin
+                for(i = 0; i < 16; i = i+1) col_buf[i] <= col_out[i];
+            end
+            CALC_DIAG: begin
                 for(i = 0; i < 16; i = i+1) keystream_buf[i] <= diagonal_out[i];
             end
             ADD: begin
